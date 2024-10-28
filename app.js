69e1069c8c94a7bbb4098e03345f122c0b250ea0
elminhuseynov <elmin.huseynov0821@gmail.com>
2024-10-28 16:41:34 +0400

    homework

---
+++ app.js
@@ -0,0 +1,15 @@
+const firstName = 'Elmin';
+const lastName = 'Huseynov';
+const greet = `Sizi bir daha görməyimizə şadıq, ${firstName} ${lastName}`;
+console.log(greet);
+
+let fullName = "Elmin Huseynov"
+console.log(fullName.length - 1);
+
+const fullNamE = "Elmin Huseynov"
+console.log(fullNamE[0] + fullNamE[6]);
+
+let text = "Elmin Huseynov"
+console.log(text.replace("Elmin Huseynov" , "Huseynov Elmin"));
+
+
